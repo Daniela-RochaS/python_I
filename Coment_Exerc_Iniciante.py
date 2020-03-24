62af90de6e577de775751ef77eda05e34ee1d12b
Daniela Rocha <danieladarochasilva16@gmail.com>
2020-03-24 12:46:08 -0300

    Primeiros Exercicios com python

---
+++ Coment_Exerc_Iniciante.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#Curso Udemy , iniciado dia 20 de março de 2020
+print("Olá Mundo!!")
+
+""" Operaçoes Matematicas
++  -  *  / 
+Módulo : oque resta % """
+print (2+2)
+
+# Variaveis e tipos de dados 
+""" permite o armazenamento de informação na memória; Ou seja, uma variável permite que uma informação seja execultada várias vezes.
+AS variaveis nao podem ter espaços ( _ )
+ - numerico inteira ou flutuante/float (decimal)
+ - textual/ string
+ - boleana (true/ false) """
+	#criar o comando Olá mundo varias vezes
+
+World = "Olá Mundo!"   #variavel
+print(World)
+print(World)           #execução variavel
+
+
+var1= 1    #variável inteira
+var2= 1.1    # variável float
+var3 = "Exemplo textual"
+var4= True
+var4=False         #boleana  (false sobressai a true por ondem de escrita)
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+
+
+""" Operadores lógicos e relacionais  
+- operadores matematicos (+;-;*;/;%) 
+- de atribuição ( = ) ex.: x+10 ; lê-se "x recebe o valor 10"           ambos são lógicos 
+
+      RELACIONAIS  (como saber se uma coisa é igual a outra)
+
+  ==	igual
+  !=	diferente
+  >		maior
+  <		menor
+  >=	maior igual
+  <=	menos igual
+
+"""
+
+x=2
+y=4
+
+x=y      #neste caso, x recebeu o valor de y
+print(x)
+
+x=2
+y=4
+
+x==y
+print(x==y)        # x não é igual a Y (FALSE) ; uma comparação
+soma= x+y
+print(soma)
+
+
+
+
+
+ 
